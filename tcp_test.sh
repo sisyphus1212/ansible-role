d83076a4e143522217b6979de8b9ea464b89a3e5
liaocj <liaocj@yusur.tech>
2024-01-05 15:03:09 +0800

    fix err

--- tcp_test.sh
+++ tcp_test.sh
@@ -1,4 +1,3 @@
-sudo su
 case_name=virtio_test
 ansible-playbook ./inventory/init.yml --extra-vars "case_name=${case_name}"
-ansible-playbook ./${case_name}.yml
+sudo ansible-playbook ./${case_name}.yml
